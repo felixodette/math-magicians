70280d0d598b90f6f5ec9468877ae9cd42a39f03
Felix Odete <felix.odette@gmail.com>
2022-06-08 16:19:32 +0300

    Calculator render

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { Component } from 'react';
+import Calculator from './components/Calculator';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return(<Calculator/>);
+  }
+}
+
+export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,27 +8,27 @@ class Calculator extends Component {
 
   render() {
     return (
-      <div className="calculator-container">
-        <div className="result">0</div>
-        <button type="button" className="calc-head-operators">AC</button>
-        <button type="button" className="calc-head-operators">+/-</button>
-        <button type="button" className="calc-head-operators">%</button>
-        <button type="button" className="calc-operators">÷</button>
-        <button type="button" className="calc-numbers">7</button>
-        <button type="button" className="calc-numbers">8</button>
-        <button type="button" className="calc-numbers">9</button>
-        <button type="button" className="calc-operators">x</button>
-        <button type="button" className="calc-numbers">4</button>
-        <button type="button" className="calc-numbers">5</button>
-        <button type="button" className="calc-numbers">6</button>
-        <button type="button" className="calc-operators">-</button>
-        <button type="button" className="calc-numbers">1</button>
-        <button type="button" className="calc-numbers">2</button>
-        <button type="button" className="calc-numbers">3</button>
-        <button type="button" className="calc-operators">+</button>
-        <button type="button" className="calc-numbers zero">0</button>
-        <button type="button" className="calc-decimal">.</button>
-        <button type="button" className="calc-operators">=</button>
+      <div className="calc-container">
+        <div className="calc-result">0</div>
+        <button type="button" className="top-operators">AC</button>
+        <button type="button" className="top-operators">+/-</button>
+        <button type="button" className="top-operators">%</button>
+        <button type="button" className="operators">÷</button>
+        <button type="button" className="numbers">7</button>
+        <button type="button" className="numbers">8</button>
+        <button type="button" className="numbers">9</button>
+        <button type="button" className="operators">x</button>
+        <button type="button" className="numbers">4</button>
+        <button type="button" className="numbers">5</button>
+        <button type="button" className="numbers">6</button>
+        <button type="button" className="operators">-</button>
+        <button type="button" className="numbers">1</button>
+        <button type="button" className="numbers">2</button>
+        <button type="button" className="numbers">3</button>
+        <button type="button" className="operators">+</button>
+        <button type="button" className="numbers zero">0</button>
+        <button type="button" className="decimal">.</button>
+        <button type="button" className="operators">=</button>
       </div>
     );
   }
