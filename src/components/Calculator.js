3d54ad590223fda404d39423e45572f469715a40
Felix Odete <felix.odette@gmail.com>
2022-06-09 01:05:57 +0300

    Calc functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,47 @@
 import React, { Component } from 'react';
 import Button from './Button';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      totalResult: '0',
+      next: null,
+      operation: null,
+    };
+    this.getResult = this.getResult.bind(this);
+  }
+
+  getResult = (event) => {
+    const result = calculate(this.state, event.target.innerText);
+    this.setState(result);
   }
 
   render() {
+    const { totalResult, operation, next } = this.state;
     return (
       <div className="calc-container">
-        <div className="calc-result">0</div>
-        <button type="button" className="top-operators">AC</button>
-        <button type="button" className="top-operators">+/-</button>
-        <button type="button" className="top-operators">%</button>
-        <button type="button" className="operators">÷</button>
-        <button type="button" className="numbers">7</button>
-        <button type="button" className="numbers">8</button>
-        <button type="button" className="numbers">9</button>
-        <button type="button" className="operators">x</button>
-        <button type="button" className="numbers">4</button>
-        <button type="button" className="numbers">5</button>
-        <button type="button" className="numbers">6</button>
-        <button type="button" className="operators">-</button>
-        <button type="button" className="numbers">1</button>
-        <button type="button" className="numbers">2</button>
-        <button type="button" className="numbers">3</button>
-        <button type="button" className="operators">+</button>
-        <button type="button" className="numbers zero">0</button>
-        <button type="button" className="decimal">.</button>
-        <button type="button" className="operators">=</button>
+        <div className="calc-result">{`${totalResult || ''} ${operation || ''} ${next || ''}`}</div>
+        <Button eventHandler={this.getResult} value="AC" />
+        <Button eventHandler={this.getResult} value="+/-" />
+        <Button eventHandler={this.getResult} value="%" />
+        <Button buttonClass="calc-operators" eventHandler={this.getResult} value="÷" />
+        <Button eventHandler={this.getResult} value="7" />
+        <Button eventHandler={this.getResult} value="8" />
+        <Button eventHandler={this.getResult} value="9" />
+        <Button buttonClass="calc-operators" eventHandler={this.getResult} value="x" />
+        <Button eventHandler={this.getResult} value="4" />
+        <Button eventHandler={this.getResult} value="5" />
+        <Button eventHandler={this.getResult} value="6" />
+        <Button buttonClass="calc-operators" eventHandler={this.getResult} value="-" />
+        <Button eventHandler={this.getResult} value="1" />
+        <Button eventHandler={this.getResult} value="2" />
+        <Button eventHandler={this.getResult} value="3" />
+        <Button buttonClass="calc-operators" eventHandler={this.getResult} value="+" />
+        <Button buttonClass="zero" eventHandler={this.getResult} value="0" />
+        <Button eventHandler={this.getResult} value="." />
+        <Button buttonClass="calc-operators" eventHandler={this.getResult} value="=" />
       </div>
     );
   }
