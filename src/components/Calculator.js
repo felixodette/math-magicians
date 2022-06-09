db93950d397d98d11ff58db0fae0a4df6844b846
Felix Odete <felix.odette@gmail.com>
2022-06-09 10:59:23 +0300

    gitflow-feature-stash: hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,37 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import Button from './Button';
+import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+      total: 0,
+      next: null,
+      operation: null,
+    });
 
-  render() {
-    return (
-      <div className="calc-container">
-        <div className="calc-result">0</div>
-        <button type="button" className="top-operators">AC</button>
-        <button type="button" className="top-operators">+/-</button>
-        <button type="button" className="top-operators">%</button>
-        <button type="button" className="operators">÷</button>
-        <button type="button" className="numbers">7</button>
-        <button type="button" className="numbers">8</button>
-        <button type="button" className="numbers">9</button>
-        <button type="button" className="operators">x</button>
-        <button type="button" className="numbers">4</button>
-        <button type="button" className="numbers">5</button>
-        <button type="button" className="numbers">6</button>
-        <button type="button" className="operators">-</button>
-        <button type="button" className="numbers">1</button>
-        <button type="button" className="numbers">2</button>
-        <button type="button" className="numbers">3</button>
-        <button type="button" className="operators">+</button>
-        <button type="button" className="numbers zero">0</button>
-        <button type="button" className="decimal">.</button>
-        <button type="button" className="operators">=</button>
-      </div>
-    );
-  }
-}
+  const calculateResult = (event) => {
+    const result = calculate(state, event.target.innerText);
+    setState(result);
+  };
+
+  const { total, next, operation } = state;
+  return (
+    <div className="calc-container">
+      <div className="calc-result">{`${total || ''} ${operation || ''} ${next || ''}`}</div>
+      <Button eventHandler={calculateResult} value="AC" />
+      <Button eventHandler={calculateResult} value="+/-" />
+      <Button eventHandler={calculateResult} value="%" />
+      <Button buttonClass="calc-operators" eventHandler={calculateResult} value="÷" />
+      <Button eventHandler={calculateResult} value="7" />
+      <Button eventHandler={calculateResult} value="8" />
+      <Button eventHandler={calculateResult} value="9" />
+      <Button buttonClass="calc-operators" eventHandler={calculateResult} value="x" />
+      <Button eventHandler={calculateResult} value="4" />
+      <Button eventHandler={calculateResult} value="5" />
+      <Button eventHandler={calculateResult} value="6" />
+      <Button buttonClass="calc-operators" eventHandler={calculateResult} value="-" />
+      <Button eventHandler={calculateResult} value="1" />
+      <Button eventHandler={calculateResult} value="2" />
+      <Button eventHandler={calculateResult} value="3" />
+      <Button buttonClass="calc-operators" eventHandler={calculateResult} value="+" />
+      <Button buttonClass="zero" eventHandler={calculateResult} value="0" />
+      <Button eventHandler={calculateResult} value="." />
+      <Button buttonClass="calc-operators" eventHandler={calculateResult} value="=" />
+    </div>
+  );
+};
 
 export default Calculator;
