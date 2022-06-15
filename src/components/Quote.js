649fc8ba6bba71c3dea1c7db3a680697122c5471
Felix Odete <felix.odette@gmail.com>
2022-06-15 20:27:35 +0300

    Eslint fix

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-<!-- eslint-disable max-len -->
 const Quote = () => (
   <div id="quote">
     <ul>
       <li>
-        &quot;In my school, the brightest boys did math and physics, the less bright did physics and chemistry,
-        and the least bright did biology. I wanted to do math and physics, but my father made me do chemistry
-        because he thought there would be no jobs for mathematicians.&quot;
+        &quot;In my school, the brightest boys did math and physics.&quot;
         <span> - Stephen Hawking</span>
       </li>
       <li>
@@ -15,23 +12,19 @@ const Quote = () => (
         <span>- Nicolaus Corpenicus</span>
       </li>
       <li>
-        &quot;The truth has never been of any real value to any human being - it is a symbol for mathematicians
-        and philosophers to pursue. In human relations kindness and lies are worth a thousand truths..&quot;
+        &quot;The truth has never been of any real value to any human being.&quot;
         <span>- Graham Greene</span>
       </li>
       <li>
-        &quot;Programming is one of the most difficult branches of applied mathematics; the poorer
-        mathematicians had better remain pure mathematicians..&quot;
+        &quot;Programming is one of the most difficult branches of applied mathematics.&quot;
         <span>- Edsger Dijkstra</span>
       </li>
       <li>
-        &quot;It has been very rare to see a black woman as a protagonist. And also as three-dimensional
-        people - mathematicians, mothers, wives, complicated people, not perfect..&quot;
+        &quot;It has been very rare to see a black woman as a protagonist.&quot;
         <span>- Margot Lee Shetterly</span>
       </li>
     </ul>
   </div>
 );
-<!-- eslint-enable max-len -->
 
 export default Quote;
