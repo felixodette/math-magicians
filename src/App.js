40494e77fa876e5373934622aa368f68e40c227a
Felix Odete <felix.odette@gmail.com>
2022-06-15 18:57:22 +0300

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import React, { Component } from 'react';
 import Calculator from './components/Calculator';
+import { Routes, Route, } from 'react-router-dom';
+import NavBar from './components/Nav';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +13,18 @@ class App extends Component {
   }
 
   render() {
-    return (<Calculator />);
+    return (
+      <>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Quote" element={<Quote />} />
+            <Route path="/Calculator" element={<Calculator />} />
+          </Routes>
+        </main>
+      </>
+    );
   }
 }
 
